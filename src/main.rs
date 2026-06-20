53be6dc9bcfbcd18b1fbe039245be3702bab2c87
c272 <dev@c272.org>
2026-06-20 20:51:33 +0900

    Bump version to v0.2.0.

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,7 @@ Terms:
   *  +<SHORTCUT>    A user-defined shortcut. See [3] for details.
 
 
-`warp-to` (v0.1.0) Copyright (C) 2026, c272
+`warp-to` (v0.2.0) Copyright (C) 2026, c272
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License v3 as published by the Free
 Software Foundation.
